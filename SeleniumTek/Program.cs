c18c382cc5402d021340b9c6d1311c658da76b7c
MUHİTTİN AKIN <muhittinbilmuh@gmail.com>
2024-03-31 04:33:40 +0300

    first commit

---
+++ SeleniumTek/Program.cs
@@ -0,0 +1,99 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SeleniumTek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //var options = new ChromeOptions();
+            //options.AddArgument("start-maximized"); 
+            //IWebDriver driver = new ChromeDriver(options);
+            //driver.Navigate().GoToUrl("https://giris.turkiye.gov.tr/Giris/gir");
+            //IWebElement tcInput = driver.FindElement(By.Id("tridField"));
+            //IWebElement sifreInput = driver.FindElement(By.Id("egpField"));
+            //tcInput.SendKeys("?");
+            //sifreInput.SendKeys("?");
+            //IWebElement girisButton = driver.FindElement(By.CssSelector(".btn-send"));
+            //girisButton.Click();
+            //Console.ReadLine();
+            //driver.Quit();
+            // Chrome WebDriver'ı başlat
+            var options = new ChromeOptions();
+            options.AddArgument("start-maximized");
+            IWebDriver driver = new ChromeDriver(options);
+
+            try
+            {
+                // N11 sitesine git
+                driver.Navigate().GoToUrl("https://www.n11.com/telefon-ve-aksesuarlari");
+
+                // Ürünlerin listesini al
+                var productElements = driver.FindElements(By.CssSelector(".productName"));
+
+                // Ürün adı ve fiyatını yazdır
+                foreach (var productElement in productElements)
+                {
+                    string productName = productElement.Text;
+
+                    // Ürün fiyatını alma
+                    string productPrice = "";
+                    try
+                    {
+                        productPrice = productElement.FindElement(By.XPath("./ancestor::div[@class='columnContent']//ins")).Text;
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        try
+                        {
+                            productPrice = productElement.FindElement(By.XPath("./ancestor::div[@class='columnContent']//span[@class='newPrice']")).Text;
+                        }
+                        catch (NoSuchElementException)
+                        {
+                            productPrice = "Fiyat bilgisi bulunamadı";
+                        }
+                    }
+
+                    // Ürün adını ve fiyatını renkli olarak yazdır
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Ürün Adı: " + productName);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ürün Fiyatı: " + productPrice);
+                    Console.ResetColor();
+                    Console.WriteLine("--------------------");
+
+                    // Pop-up penceresinde ürün adı ve fiyatını göster
+                    ShowPopup(productName, productPrice);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+            finally
+            {
+                // Tarayıcıyı kapat
+                driver.Quit();
+                Console.ReadLine();
+            }
+        }
+
+        // Pop-up penceresinde ürün adı ve fiyatını gösteren metod
+        static void ShowPopup(string productName, string productPrice)
+        {
+            // Burada pop-up penceresi açılabilir, örneğin Windows Forms veya WPF uygulamasında
+            // Aşağıdaki kod örnek amaçlıdır ve gerçek bir pop-up penceresi açmaz
+            Console.WriteLine("Pop-up: " + productName + " - " + productPrice);
+            
+        }
+    }
+}
---
+++ packages/Selenium.WebDriver.MSEdgeDriver.122.0.2365.63/README.md
@@ -0,0 +1,67 @@
+# DISCLAIMER: Maybe this package is not for you
+
+From v4.0, Selenium has Selenium Manager (https://www.selenium.dev/documentation/selenium_manager/):
+
+```
+Selenium Manager is a command-line tool implemented in Rust that provides automated driver and browser management for Selenium. Selenium bindings use this tool by default, so you do not need to download it or add anything to your code or do anything else to use it
+```
+
+# Install the NuGet Package
+
+```
+Install-Package Selenium.WebDriver.MSEdgeDriver
+or
+dotnet add package Selenium.WebDriver.MSEdgeDriver
+```
+
+# Getting started
+
+```
+var edgeOptions = new EdgeOptions
+{
+    UseChromium = true
+};
+var edgeDriverService = EdgeDriverService.CreateChromiumService();
+```
+
+For more information, see https://github.com/microsoft/edge-selenium-tools.
+
+# Create the NuGet package
+
+```
+getPackage.bat <version>
+```
+
+For example, to automatically download the Microsoft Edge Driver and create the NuGet package '.\dist\Selenium.WebDriver.MSEdgeDriver.84.0.522.49.nupkg':
+
+```
+getPackage.bat 84.0.522.49
+```
+
+# Contributing
+
+Most the process is automated, but I still need to check MS Edge Driver's page and execute the batch file manually. I am trying to do this once a month. If you would like to have faster releases, please contact me to become a contributor.
+
+
+# Cross-platform building and publishing
+
+Edge driver developers normally releases versions with win32, win64, mac and linux binaries, but sometimes they decide to release one version with just linux, or just mac and windows binaries. 
+The automated process can only make nuget packages of full releases.
+
+# How to include the driver file into published files?
+
+"msedgedriver(.exe)" isn't included in published files on default configuration. This behavior is by design and follows https://raw.githubusercontent.com/jsakamoto/nupkg-selenium-webdriver-chromedriver/ design. For more info reffer to guides at https://raw.githubusercontent.com/jsakamoto/nupkg-selenium-webdriver-chromedriver.
+
+To include published files, define `PublishMsEdgeDriver` property with value is "true" in MSBuild file (.csproj, .vbproj, etc...) to publish the driver file instead of define compilation symbol.
+
+```xml
+  <Project ...>
+    ...
+    <PropertyGroup>
+      ...
+      <PublishMsEdgeDriver>true</PublishMsEdgeDriver>
+      ...
+    </PropertyGroup>
+...
+</Project>
+```
